6ac1e69ce138b6516cf9acff8671e7d03761d09d
X <Nhlapho7@gmail.com>
2020-12-02 12:32:42 +0100

    Revision
    I learned about the Basics and tools of computer graphics, MouseHandling tools.

---
+++ IntroToCompGraphicsPractise/IntroToCompGraphicsPractise/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicsAndTools
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Point p1, p2, center;
+        Point[] points;
+        public Form1()
+        {
+            InitializeComponent();
+            Point p1 = new Point(100,200);
+            Point p2 = new Point(400, 500);
+            points = new Point[] { new Point(100,300), new Point(300, 700), new Point(700, 500), 
+                                    new Point(500, 145), new Point(145, 400), };
+            center = new Point(Canvas.Width/2, Canvas.Height/2); 
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+
+            Pen black = new Pen(Color.Black);
+            //Line
+            g.DrawLine(black, 100, 200, 400, 500);
+            g.DrawLines(black, points);
+            g.DrawLine(Pens.Black, 0, center.Y, Canvas.Width, center.Y);
+            g.DrawLine(Pens.Green, new PointF(0, 0), center);
+
+            //Rectangle and Ellipse
+            g.FillRectangle(new SolidBrush(Color.Yellow), 100, 100, 200, 300);
+            g.DrawRectangle(black, 100, 100, 200, 300);
+            g.FillEllipse(Brushes.Red, 100, 100, 200, 300);
+            g.DrawEllipse(Pens.Magenta, 100, 100, 200, 300);
+
+            g.DrawRectangle(Pens.Brown, 100, 100, 0.5f, 0.5f);
+            float r = 15;
+            g.DrawEllipse(Pens.Red, 600 - r, 100 - r, 2 * r, 2 * r);
+
+            for (int i = 0; i < 256; i++)
+                for (int j = 0; j < 256; j++)
+                    g.DrawRectangle(new Pen(Color.FromArgb(i, j, j)),
+                        center.X + i, center.Y + j, 0.5f, 0.5f);
+
+        }
+        #region Mouse handling
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ IntroToCompGraphicsPractise/MouseHandling/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseHandling
+{
+    public partial class Form1 : Form
+    {
+        Brush brush = new SolidBrush(Color.Red);
+        PointF p1;
+        Graphics g;
+        int size = 200;
+        float speed = 3;
+        bool gotcha = false;
+        int dirX = 1, dirY = 1;
+        float dx, dy;
+        public Form1()
+        {
+            InitializeComponent();
+            p1 = new PointF(100, 400);
+            timer.Start();
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.FillRectangle(brush, p1.X, p1.Y, size, size);
+        }
+
+        #region Mouse Handling
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            speed += 0.5f;
+            size -= 10;
+            gotcha = false;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (gotcha)
+            {
+                p1 = new PointF(e.X - dx, e.Y - dy);
+
+                Canvas.Refresh();
+            }
+        }
+
+        private void timer_Tick_1(object sender, EventArgs e)
+        {
+            if (!gotcha)
+            {
+                p1 = new PointF(p1.X + dirX * speed, p1.Y + dirY * speed);
+
+                if (p1.Y + size >= Canvas.Height)
+                {
+                    p1.Y = Canvas.Height - size - 1;
+                    dirY *= -1;
+                }
+                if (p1.Y <= 0)
+                {
+                    p1.Y = 1;
+                    dirY *= -1;
+                }
+                //HW.: Vertical check
+                if (p1.X + size >= Canvas.Width)
+                {
+                    p1.X = Canvas.Width - size - 1;
+                    dirX *= -1;
+                }
+                if (p1.X <= 0)
+                {
+                    p1.X = 1;
+                    dirX *= -1;
+                }
+
+                Canvas.Refresh();
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(p1.X<= e.X && e.X <= p1.X + size && p1.Y <= e.Y && e.Y <= p1.Y + size)
+            {
+                if (size == 150)
+                {
+                    timer.Stop();
+                    MessageBox.Show("You win!");
+                }
+
+                dx = e.X - p1.X;
+                dy = e.Y - p1.Y;
+                gotcha = true;
+            }
+        }
+        #endregion
+        
+    }
+}
